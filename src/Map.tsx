23119aded27b75a1538d51e2e51253aa260c95ee
Migouda-linux <malekabdelkader2@gmail.com>
2022-05-20 16:22:14 +0200

    chore: convert Map to tsx

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,38 +1,30 @@
-
-// @ts-nocheck
-
 import mapboxgl from 'mapbox-gl';
 import React, { useEffect, useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
+import Marker from './components/Marker';
 import './Map.css';
+import GeoJson from './models/geoJson.model';
 
 
 mapboxgl.accessToken =
   'pk.eyJ1IjoibWFsZWs5NyIsImEiOiJjbDNlYXMyb2MwZ2puM21wcjE3ZGx6dHljIn0.khxYSNnUEhDCeR00mCl8EQ';
 
-/** 
- * the Marker is using a Javscript Library not a react Library 
- * 
- */
-const Marker = ({ onClick, children, car ,selectedCarId}) => {
-  const _onClick = (e) => {
-    onClick(car.id);
-  };
-  return (
-    <button onClick={_onClick} className="marker">
-      <img height={"70px"} src='https://www.shareicon.net/data/512x512/2016/01/15/703693_gps_512x512.png'></img>
-      {(selectedCarId==car.id) && <p>{car.driver.name}</p> }
-      {children}
-    </button>
-  );
-};
 
-const Map = ({geoJson,lng,lat,zoom,selectedCarId,setselectedCarId}) => {
-  const mapContainerRef = useRef(null);
-  const [map,setmap]=useState(null)
-  //Object that hold all the existant map markers 
-  const [markers,setMarkers]=useState({})
-  
+
+  interface MapProps{
+    geoJson:GeoJson,
+    lng:number,
+    lat:number,
+    zoom:number,
+    selectedCarId:string,
+    setselectedCarId: React.Dispatch<React.SetStateAction<string>>
+  }
+  const Map:React.FC<MapProps> = ({geoJson,lng,lat,zoom,selectedCarId,setselectedCarId}) => {
+    const mapContainerRef =useRef<HTMLDivElement | null>(null);
+    const [map,setmap]=useState<mapboxgl.Map>()
+    //Object that hold all the existant map markers 
+    const [markers,setMarkers]=useState<{[e:string]:mapboxgl.Marker}>({})
+    const ref = React.useRef<any>();
 
   //Update the map View when map lat,lng,zoom changes 
   useEffect(() => {
@@ -45,6 +37,9 @@ const Map = ({geoJson,lng,lat,zoom,selectedCarId,setselectedCarId}) => {
 
   // Initialize map when component mounts
   useEffect(() => {
+    if(mapContainerRef.current){
+
+    
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
       style: 'mapbox://styles/mapbox/streets-v11',
@@ -56,13 +51,12 @@ const Map = ({geoJson,lng,lat,zoom,selectedCarId,setselectedCarId}) => {
     // Render custom marker components
     geoJson.cars.forEach((car) => {
       // Create a React ref
-      const ref = React.createRef();
       // Create a new DOM node and save it to the React ref
       ref.current = document.createElement('div');
       ref.current.id=car.id
       // Render a Marker Component on our new DOM node
       ReactDOM.render(
-        <Marker onClick={markerClicked} car={car} setselectedCarId={setselectedCarId} selectedCarId={selectedCarId}/>,
+        <Marker onClick={markerClicked} car={car} selectedCarId={selectedCarId}/>,
         ref.current
       );
       
@@ -81,7 +75,8 @@ const Map = ({geoJson,lng,lat,zoom,selectedCarId,setselectedCarId}) => {
 
     // Clean up on unmount
     return () => map.remove();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  }
+}, []); // eslint-disable-line react-hooks/exhaustive-deps
 
 
   // we're not using React VirtualDom
@@ -90,13 +85,12 @@ const Map = ({geoJson,lng,lat,zoom,selectedCarId,setselectedCarId}) => {
   useEffect(()=>{
     map && geoJson.cars.forEach((car) => {
       // Create a React ref
-      const ref = React.createRef();
       // Create a new DOM node and save it to the React ref
       ref.current = document.createElement('div');
       ref.current.id=car.id
       // Render a Marker Component on our new DOM node
       ReactDOM.render(
-        <Marker onClick={markerClicked} car={car} setselectedCarId={setselectedCarId} selectedCarId={selectedCarId}/>,
+        <Marker onClick={markerClicked} car={car} selectedCarId={selectedCarId}/>,
         ref.current
       );
       //remove the old marker from the map
@@ -116,16 +110,16 @@ const Map = ({geoJson,lng,lat,zoom,selectedCarId,setselectedCarId}) => {
   useEffect(()=>{
     geoJson.cars.forEach(car=>{
       //change a marker position when a car coordiantes changes
-      markers[car.id] && markers[car.id].setLngLat(car.geometry.coordinates)
+      (map && markers[car.id])&& markers[car.id].setLngLat(car.geometry.coordinates)
       .addTo(map);
    })
  },[geoJson,markers])
 
  
-  const markerClicked = (id) => {
+  const markerClicked = (id:string) => {
     setselectedCarId(id)
     const selectedCarElement=document.getElementById('car-'+id)
-    selectedCarElement.scrollIntoView({behavior: "smooth", inline: "end",block:'center'})
+    selectedCarElement?.scrollIntoView({behavior: "smooth", inline: "end",block:'center'})
   };
 
   return (
---
+++ src/components/Marker.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Car from "../models/car.model";
+
+interface MarkerProps{
+    onClick:(e:string)=>void
+    car: Car
+    selectedCarId:string
+  }
+  
+const Marker:React.FC<MarkerProps> = ({ onClick,  car ,selectedCarId}) => {
+    const _onClick = (e:any) => {
+      onClick(car.id);
+    };
+    return (
+      <button onClick={_onClick} className="marker">
+        <img height={"70px"} src='https://www.shareicon.net/data/512x512/2016/01/15/703693_gps_512x512.png'></img>
+        {(selectedCarId==car.id) && <p>{car.driver.name}</p> }
+      </button>
+    );
+  };
+  export default Marker;
